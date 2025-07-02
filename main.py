45d00893a50c4771e99998b0ba817afb86ec3ae8
Vladimir <a1020706@gmail.com>
2025-07-02 17:57:49 -0500

    Conexión básica a API-Football

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+'''
 from flask import Flask
 import os
 
@@ -10,3 +12,37 @@ def ejecutar():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Render usa una variable PORT
     app.run(host='0.0.0.0', port=port)        # Escucha desde cualquier IP
+'''
+
+from flask import Flask
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/run')
+def ejecutar():
+    api_key = os.getenv("API_FOOTBALL_KEY")
+    if not api_key:
+        return "❌ No se encontró la API key"
+
+    url = "https://v3.football.api-sports.io/status"
+
+    headers = {
+        "x-apisports-key": api_key
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return f"✅ Conectado a API-Football: {response.json()['response']['account']['firstname']}"
+    else:
+        return f"❌ Error al conectar: {response.status_code} - {response.text}"
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
