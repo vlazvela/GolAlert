18c5f7488a46f816517242c973089de49ad837a2
Vladimir <a1020706@gmail.com>
2025-07-02 18:09:13 -0500

    Ruta para listar ligas

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-
-'''
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-@app.route('/run')
-def ejecutar():
-    return "✅ Script ejecutado correctamente desde Flask"
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Render usa una variable PORT
-    app.run(host='0.0.0.0', port=port)        # Escucha desde cualquier IP
-'''
-
 from flask import Flask
 import os
 import requests
@@ -41,6 +25,34 @@ def ejecutar():
         return f"✅ Conectado a API-Football: {response.json()['response']['account']['firstname']}"
     else:
         return f"❌ Error al conectar: {response.status_code} - {response.text}"
+
+
+@app.route('/ligas')
+def listar_ligas():
+    api_key = os.getenv("API_FOOTBALL_KEY")
+    url = "https://v3.football.api-sports.io/leagues"
+
+    headers = {
+        "x-apisports-key": api_key
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        ligas = response.json()["response"]
+        resumen = []
+
+        for liga in ligas:
+            resumen.append({
+                "id": liga["league"]["id"],
+                "nombre": liga["league"]["name"],
+                "pais": liga["country"]["name"],
+                "temporada": liga["seasons"][-1]["year"]  # Última temporada disponible
+            })
+
+        return {"ligas": resumen[:50]}  # Muestra solo las 50 primeras para empezar
+    else:
+        return f"❌ Error al obtener ligas: {response.status_code}"
 
 
 if __name__ == '__main__':
